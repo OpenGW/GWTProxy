fa3dbc324730c76e7fa61a3c392c65ed4ea88fd2
OpenGW <opengw@fastmail.com>
2017-10-23 01:08:28 +0800

    Setup CMake project structure.

---
+++ src/main.cxx
@@ -0,0 +1,8 @@
+#include <config.h>
+#include <cstdio>
+
+int main(int argc, char* argv[])
+{
+    printf("GWTProxy: v%d.%d.%d\n", GWTPROXY_VERSION_MAJOR, GWTPROXY_VERSION_MINOR, GWTPROXY_VERSION_PATCH);
+    return 0;
+}
